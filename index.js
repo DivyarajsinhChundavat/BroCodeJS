f5461043bdca237c9ca9ab0fad0eccf190fdd0e2
DivyarajsinhChundavat <divyarajsinhchundavat2712@gmail.com>
2024-09-22 00:52:56 +0530

    Last Practice Files

---
+++ index.js
@@ -0,0 +1,10 @@
+// fetch("names.json").then(response => response.json()).then(data => console.log(data)).catch(error => console.error(error));
+// fetch and response.json() both returns "PROMISES".
+
+//fetch is asynchronous, doesn't stop execution of the other code while it waits for the response from a server.
+
+//once server responds promise is either resolved or rejected.
+
+async function func() {
+
+}
